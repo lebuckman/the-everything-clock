c3147cce5ef21cdede5a3a393eeb023e4d44cabd
lebuckman <liambuckman0@gmail.com>
2025-08-04 14:28:44 -0700

    Add mobile sidebar

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
 dayjs.extend(dayjs_plugin_duration);
 
-// light / dark mode logic
+/**
+ * SIDEBAR NAVIGATION
+ */
+
+const navbar = document.querySelector("#navbar");
+const openBtn = document.querySelector("#open-sidebar-btn");
+const media = window.matchMedia("(width < 700px)");
+
+const navLinks = document.querySelectorAll("nav a");
+
+// close sidebar when a selection is made
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        closeSidebar();
+    });
+});
+
+// if on mobile device, make navigation untabbable when not expanded
+media.addEventListener("change", (e) => updateNavbar(e));
+
+function updateNavbar(e) {
+    const isMobile = e.matches;
+    console.log(isMobile);
+    if (isMobile) {
+        navbar.setAttribute("inert", "");
+    } else {
+        navbar.removeAttribute("inert", "");
+    }
+}
+
+function openSidebar() {
+    navbar.classList.add("show");
+    openBtn.setAttribute("aria-expanded", "true");
+    navbar.removeAttribute("inert", "");
+}
+
+function closeSidebar() {
+    navbar.classList.remove("show");
+    openBtn.setAttribute("aria-expanded", "false");
+    navbar.setAttribute("inert", "");
+}
+
+updateNavbar(media);
+
+/**
+ * LIGHT / DARK MODE
+ */
 
 let lightmode = localStorage.getItem("lightmode");
 const themeSwitch = document.querySelector("#theme-switch");
