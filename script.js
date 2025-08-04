4020139e2e604a62dd4a6b0ae4baae6314646ef0
lebuckman <liambuckman0@gmail.com>
2025-08-03 21:11:10 -0700

    Add light/dark mode toggle

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 dayjs.extend(dayjs_plugin_duration);
+
+// light / dark mode logic
+
+let lightmode = localStorage.getItem("lightmode");
+const themeSwitch = document.querySelector("#theme-switch");
+
+if (lightmode === "active") enableLightMode();
+
+themeSwitch.addEventListener("click", () => {
+    lightmode = localStorage.getItem("lightmode");
+    lightmode !== "active" ? enableLightMode() : disableLightMode();
+});
+
+function enableLightMode() {
+    document.body.classList.add("lightmode");
+    localStorage.setItem("lightmode", "active");
+}
+
+function disableLightMode() {
+    document.body.classList.remove("lightmode");
+    localStorage.setItem("lightmode", null);
+}
